2ae784d8acd6d8297ed398fcf294302b9a7ac84f
Janakiramaraju Vegiraju <Janakiramaraju.X.Vegiraju@kp.org>
2022-04-03 15:55:50 -0400

    schema validation

---
+++ block-BNaacj/models/user.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: String,
+    age: String,
+    password: {type: String, min:5, max:10},
+    createdAt: {type: Date, default: Date.now}
+})
