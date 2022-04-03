8530c5d853ec34aa56afeb5c49588263d235f33b
Janakiramaraju Vegiraju <Janakiramaraju.X.Vegiraju@kp.org>
2022-04-03 13:17:04 -0400

    Schema files

---
+++ block-BNaacf/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/blogs',(err) => {
+    err?console.log(err):console.log('Database connected Successfully');
+})
+
+const app = express();
+
+app.listen('4000', () => {
+    console.log('Server is listening on port 4000');
+})
